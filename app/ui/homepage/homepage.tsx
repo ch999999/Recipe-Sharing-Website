e64f47ace90598a4b0e4dfa7f9d72280ff5b4267
ch999999 <izakchordiscord@gmail.com>
2024-04-27 16:44:44 +0800

    removed obsolete files

--- app/ui/homepage/homepage.tsx
+++ app/ui/homepage/homepage.tsx
@@ -2,8 +2,6 @@
 import WelcomeNote from "./welcomeNote";
 import HowToCreate from "./howToCreate";
 import PlannedChanges from "./plannedChanges";
-import UserRecipes from "./userRecipes";
-import { Recipe } from "@/app/lib/definitions";
 
 export default function HomePage({isLoggedIn}:{isLoggedIn:boolean}){
     if(!isLoggedIn){
